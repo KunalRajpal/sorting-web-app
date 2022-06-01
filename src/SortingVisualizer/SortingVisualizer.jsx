8cf2f44aa703273796d1b5841ce29fe2156538ab
Kunal <rajpal.kunal1@gmail.com>
2022-06-01 03:11:39 -0500

    added the reset array and random int functions

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,24 +16,32 @@ const SECONDARY_COLOR = 'red';
 
 
 export default class SortingVisualizer extends React.component {
-
+    //array stored in our state 
+    //this is going to be our main array
     constructor(props){
         this.state = {
             array: [],
         };
     }
-
+    //when this component loads for the first time we call the reset array function to reset our array
     componentDidMount(){
         this.resetArray();
     }
 
     resetArray(){
         const array = [];
+
         for(let i = 0; i < NUMBER_OF_ARRAY_BARS; i++){
-    
+            //pushes a random value between 5 and 100 into the array
+            array.push(randomIntFromInterval(5,1000));
         }
+        this.setState({array});
     }
 
 }
 
-
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+    //inclusive of min and max
+    return Math.floor(Math.random() * (max - min +1) + min);
+}
